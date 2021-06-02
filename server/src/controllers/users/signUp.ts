414f977bf657b413c83b5b24ebc74b9457d1f8b3
Gninninwoky Ouattara <idrissgouattara@gmail.com>
2021-06-02 21:39:48 +0000

    check for email availability

--- server/src/controllers/users/signUp.ts
+++ server/src/controllers/users/signUp.ts
@@ -4,9 +4,24 @@ import { RequestHandler } from "express";
 import jwt from "jsonwebtoken";
 
 const signUp: RequestHandler = async (req, res, next) => {
-    console.log(req.body);
-    const password = await bcrypt.hashSync(req.body.password, 10);
+    const { email } = req.body;
     let user;
+    // Check for email availability
+    try {
+        user = await User.findOne({ email });
+        if (user) {
+            return res.json({
+                message: "Email already in use, please change it.",
+            });
+        }
+    } catch (error) {
+        next(new Error(error.message));
+    }
+
+    // Password hashing
+    const password = await bcrypt.hashSync(req.body.password, 10);
+
+    // Account creation
     try {
         user = await User.create({
             ...req.body,
@@ -15,9 +30,10 @@ const signUp: RequestHandler = async (req, res, next) => {
     } catch (error) {
         next(error);
     }
-
+    // Token generation
     let token = jwt.sign({ ...user }, process.env.SECRET as string);
-    res.json({ user, token });
+
+    return res.json({ user, token });
 };
 
 export default signUp;
