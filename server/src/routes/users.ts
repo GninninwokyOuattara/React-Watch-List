8e6b0a76c8b8d5038b89f78db28b8c97e173d724
Gninninwoky Ouattara <idrissgouattara@gmail.com>
2021-06-02 21:37:53 +0000

    signup and login routes

--- server/src/routes/users.ts
+++ server/src/routes/users.ts
@@ -1,10 +1,10 @@
 import Router from "express";
+import signUp from "../controllers/users/signUp";
+import login from "../controllers/users/login";
 
 const router = Router();
 
-router.post("/signup", (req, res, next) => {
-    console.log(req.body);
-    res.json(req.body);
-});
+router.post("/signup", signUp);
+router.post("/login", login);
 
 export default router;
