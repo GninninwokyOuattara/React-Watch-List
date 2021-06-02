46fd58b4f0e7defbf2867b5b60796cb866c55444
Gninninwoky Ouattara <idrissgouattara@gmail.com>
2021-06-02 21:15:05 +0000

    login controllers

---
+++ server/src/controllers/users/login.ts
@@ -0,0 +1,32 @@
+import User from "../../db/schemas/UserSchema";
+import bcrypt from "bcryptjs";
+import { RequestHandler } from "express";
+import jwt from "jsonwebtoken";
+
+const login: RequestHandler = async (req, res, next) => {
+    let user;
+    const { email, password } = req.body;
+    try {
+        user = await User.findOne({ email });
+        if (user) {
+            let isValidPassword = await bcrypt.compare(password, user.password);
+            if (isValidPassword) {
+                let token = jwt.sign({ ...user }, process.env.SECRET as string);
+                res.json({
+                    message: "Connected",
+                    user: {
+                        id: user._id,
+                        email: user.email,
+                        token,
+                    },
+                });
+            } else {
+                res.status(500).json({ message: "Invalid Password" });
+            }
+        }
+    } catch (error) {
+        next(new Error(error.message));
+    }
+};
+
+export default login;
