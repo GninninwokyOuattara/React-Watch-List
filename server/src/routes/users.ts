ace9764419f4bb750b108b377c50f3f17ff848bc
Gninninwoky Ouattara <idrissgouattara@gmail.com>
2021-06-03 01:06:06 +0000

    add endpoint to remove a movie from watchlist by its id inside the db

--- server/src/routes/users.ts
+++ server/src/routes/users.ts
@@ -2,12 +2,13 @@ import Router from "express";
 import signUp from "../controllers/users/signUp";
 import login from "../controllers/users/login";
 import addToWatchlist from "../controllers/users/add";
+import removeFromWatchlistById from "../controllers/users/removeFromWatchlistById";
 
 const router = Router();
 
 router.post("/signup", signUp);
 router.post("/login", login);
 router.post("/:userid/add", addToWatchlist);
-router.post("/:userid/delete");
+router.delete("/:userid/:movieid", removeFromWatchlistById);
 
 export default router;
