95e463c2927d463f5ad22865392a423343bc6338
Gninninwoky Ouattara <idrissgouattara@gmail.com>
2021-06-03 15:18:25 +0000

    two new route and comment telling what their role is

--- server/src/routes/users.ts
+++ server/src/routes/users.ts
@@ -2,13 +2,28 @@ import Router from "express";
 import signUp from "../controllers/users/signUp";
 import login from "../controllers/users/login";
 import addToWatchlist from "../controllers/users/add";
+import getAllUsers from "../controllers/users/getAllUsers";
+import getUserById from "../controllers/users/getUserById";
 import removeFromWatchlistById from "../controllers/users/removeFromWatchlistById";
 
 const router = Router();
 
+// Fetch all users
+router.get("/", getAllUsers);
+
+// Fetch user data including his wathclist using the user id
+router.get("/:userid", getUserById);
+
+// Signup user
 router.post("/signup", signUp);
+
+// User login
 router.post("/login", login);
+
+// User adding a movie to his watchlist
 router.post("/:userid/add", addToWatchlist);
+
+// User removing a movie from his wathclist using the id
 router.delete("/:userid/:movieid", removeFromWatchlistById);
 
 export default router;
