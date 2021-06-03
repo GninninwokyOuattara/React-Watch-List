9a4857538c9b7a3933ec5ce44ec98b4d8f29f89e
Gninninwoky Ouattara <idrissgouattara@gmail.com>
2021-06-03 14:04:12 +0000

    images server

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,5 +1,6 @@
 import express, { Express } from "express";
 import cors from "cors";
+import path from "path";
 
 // Database connection
 import "./db/mongoose";
@@ -12,6 +13,8 @@ const app: Express = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors());
+
+app.use("/images", express.static(path.join(__dirname, "images")));
 
 app.use("/api/users/", userRouter);
 
