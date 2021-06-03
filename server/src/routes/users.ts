4eab4ade18a6bbec190b8f7a4790b4e82ea474ad
Gninninwoky Ouattara <idrissgouattara@gmail.com>
2021-06-03 00:39:29 +0000

    add controller for add endpoint

--- server/src/routes/users.ts
+++ server/src/routes/users.ts
@@ -1,10 +1,13 @@
 import Router from "express";
 import signUp from "../controllers/users/signUp";
 import login from "../controllers/users/login";
+import addToWatchlist from "../controllers/users/add";
 
 const router = Router();
 
 router.post("/signup", signUp);
 router.post("/login", login);
+router.post("/:userid/add", addToWatchlist);
+router.post("/:userid/delete");
 
 export default router;
