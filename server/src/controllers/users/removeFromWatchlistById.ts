76ca53e8f3b61ed965c356dc2886dcae32d1ad6a
Gninninwoky Ouattara <idrissgouattara@gmail.com>
2021-06-04 15:16:23 +0000

    user data from auth middle, remove password from response object

--- server/src/controllers/users/removeFromWatchlistById.ts
+++ server/src/controllers/users/removeFromWatchlistById.ts
@@ -1,26 +1,22 @@
 import { RequestHandler } from "express";
 import mongoose from "mongoose";
+
 import ErrorWithStatusCode from "./../../utils/customError";
+import { RequestWithUser } from "../middleware/authentification";
 
 import Movie from "../../db/schemas/MovieSchema";
 import User from "../../db/schemas/UserSchema";
 
-const removeFromWatchlistById: RequestHandler = async (req, res, next) => {
-    const { userid, movieid } = req.params;
-
-    let user;
-    // Check user existence
-    try {
-        user = await User.findOne({ _id: userid });
-        if (!user) {
-            throw new ErrorWithStatusCode("User Not Found", 404);
-        }
-    } catch (error) {
-        return next(error);
-    }
+const removeFromWatchlistById: RequestHandler = async (
+    req: RequestWithUser,
+    res,
+    next
+) => {
+    const { movieid } = req.params;
+    let { user } = req as { user: any };
 
     // Check for presence of the movie id inside user watchlist
-    if (!user.watchlist.includes(movieid)) {
+    if (!user!.watchlist.includes(movieid)) {
         return res.status(404).json({ message: "Movie not in watchlist" });
     }
 
@@ -38,14 +34,17 @@ const removeFromWatchlistById: RequestHandler = async (req, res, next) => {
     try {
         const session = await mongoose.startSession();
         session.startTransaction();
-        user.watchlist.pull(movie._id);
-        await user.save({ session });
-        movie.users.pull(user._id);
+        user!.watchlist.pull(movie._id);
+        await user!.save({ session });
+        movie.users.pull(user!._id);
         await movie.save({ session });
         session.commitTransaction();
     } catch (error) {
         return next(error);
     }
+
+    user = user!.toObject();
+    delete user!.password;
 
     return res.json({
         message: "Movie successfully removed from your watchlist",
