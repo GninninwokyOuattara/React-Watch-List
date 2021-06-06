d08176d674f38b6b68523f36edb221e90cabc241
Gninninwoky Ouattara <idrissgouattara@gmail.com>
2021-06-06 23:22:32 +0000

    hide/reveal link depending on user authentification state

--- client/src/components/navigation/MainNavigation.tsx
+++ client/src/components/navigation/MainNavigation.tsx
@@ -1,9 +1,13 @@
-import React from "react";
+import React, { useContext } from "react";
 
 import { NavLink } from "react-router-dom";
 import Burger from "./Burger";
 
+import { Auth } from "../../context/AuthContext";
+
 const MainNavigation = () => {
+    const auth = useContext(Auth);
+
     return (
         <div className="w-screen h-14 bg-gray-600 flex justify-between items-center px-8 py-2 mb-1">
             <Burger />
@@ -12,9 +16,21 @@ const MainNavigation = () => {
                 <NavLink to="/" className="mx-3">
                     My Watchlist
                 </NavLink>
-                <NavLink to="/login" className="mx-3">
-                    Login
-                </NavLink>
+                {!auth?.isLoggedIn && (
+                    <React.Fragment>
+                        <NavLink to="/login" className="mx-3">
+                            Login
+                        </NavLink>
+                        <NavLink to="/register" className="mx-3">
+                            Register
+                        </NavLink>
+                    </React.Fragment>
+                )}
+                {auth?.isLoggedIn && (
+                    <NavLink to="/" className="mx-3">
+                        Logout
+                    </NavLink>
+                )}
             </ul>
         </div>
     );
