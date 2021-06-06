07434e0782ece25c3bc0838e80b0a8495c5206d9
Gninninwoky Ouattara <idrissgouattara@gmail.com>
2021-06-06 15:11:31 +0000

    button component basic

--- client/src/components/shared/Button.tsx
+++ client/src/components/shared/Button.tsx
@@ -1,7 +1,18 @@
 import React from "react";
 
-const Button = () => {
-    return <div></div>;
+interface props {
+    children: string;
+    bottom?: boolean;
+}
+const Button: React.FC<props> = (props) => {
+    return (
+        <button
+            type="submit"
+            className="bg-yellow-400 w-full h-10 mt-auto text-white rounded-md font-mono text-xl"
+        >
+            {props.children}
+        </button>
+    );
 };
 
 export default Button;
