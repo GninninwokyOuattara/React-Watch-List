6b3149f6dc8bfb4653cbe23efbea662d338a88e2
Gninninwoky Ouattara <idrissgouattara@gmail.com>
2021-06-06 12:03:09 +0000

    very basic setup of routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+    BrowserRouter as Router,
+    Route,
+    Redirect,
+    Switch,
+} from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import logo from "./logo.svg";
+import "./App.css";
+
+import LoginPage from "./pages/Login";
+import MainNavigation from "./components/navigation/MainNavigation";
+
+const App = () => {
+    return (
+        <Router>
+            <MainNavigation />
+            <main>
+                <Switch>
+                    <Route path="/" exact>
+                        <h1>Index Page</h1>
+                    </Route>
+                    <Route path="/login" exact>
+                        <LoginPage />
+                    </Route>
+                </Switch>
+            </main>
+        </Router>
+    );
+};
 
 export default App;
