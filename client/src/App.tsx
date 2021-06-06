ada6c2faeaa867af442c4d17844c0187ef111a84
Gninninwoky Ouattara <idrissgouattara@gmail.com>
2021-06-06 22:55:14 +0000

    conditionnal routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useContext } from "react";
 import {
     BrowserRouter as Router,
     Route,
@@ -10,22 +10,43 @@ import logo from "./logo.svg";
 import "./App.css";
 
 import LoginPage from "./pages/Login";
+import RegisterPage from "./pages/SignIn";
 import MainNavigation from "./components/navigation/MainNavigation";
+import { Auth } from "./context/AuthContext";
 
 const App = () => {
+    const auth = useContext(Auth);
+
+    let router;
+    if (auth?.isLoggedIn) {
+        router = (
+            <Switch>
+                <Route path="/" exact>
+                    <h1>Index Page</h1>
+                </Route>
+                <Redirect to="/" />
+            </Switch>
+        );
+    } else {
+        router = (
+            <Switch>
+                <Route path="/" exact>
+                    <h1>Index Page</h1>
+                </Route>
+                <Route path="/login" exact>
+                    <LoginPage />
+                </Route>
+                <Route path="/register" exact>
+                    <RegisterPage />
+                </Route>
+            </Switch>
+        );
+    }
+
     return (
         <Router>
             <MainNavigation />
-            <main>
-                <Switch>
-                    <Route path="/" exact>
-                        <h1>Index Page</h1>
-                    </Route>
-                    <Route path="/login" exact>
-                        <LoginPage />
-                    </Route>
-                </Switch>
-            </main>
+            <main>{router}</main>
         </Router>
     );
 };
