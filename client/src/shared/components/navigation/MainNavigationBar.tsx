7f76392c3a189c1a2d5000f9674a68dd9af512e2
Gninninwoky Ouattara <idrissgouattara@gmail.com>
2021-06-08 15:38:41 +0000

    dynamic link

--- client/src/shared/components/navigation/MainNavigationBar.tsx
+++ client/src/shared/components/navigation/MainNavigationBar.tsx
@@ -1,5 +1,7 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Logo from "../Logo";
+import NavigationItem from "./NavigationItem";
+import Links from "./navigationLink";
 
 interface props {
     children: Element[];
@@ -7,8 +9,18 @@ interface props {
 
 const MainNavigationBar = () => {
     return (
-        <div className="bg-black h-full w-64 text-white">
+        <div className="bg-black h-full w-64 text-white flex flex-col items-center mr-2">
             <Logo />
+
+            <ul>
+                {Links.map((link) => {
+                    return (
+                        <li>
+                            <NavigationItem to={link.to} text={link.link} />
+                        </li>
+                    );
+                })}
+            </ul>
         </div>
     );
 };
