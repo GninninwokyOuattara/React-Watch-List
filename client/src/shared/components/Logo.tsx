68596a427368b9b71f0c0a4bc9af132be6a087d9
Gninninwoky Ouattara <idrissgouattara@gmail.com>
2021-06-08 15:39:23 +0000

    wrapped with Navlink component

--- client/src/shared/components/Logo.tsx
+++ client/src/shared/components/Logo.tsx
@@ -1,12 +1,15 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 
 const Logo = () => {
     return (
-        <div className="w-full h-12 flex items-center justify-center my-2">
-            <div className="text-blue-500 bg-yellow-300 w-20 h-10 flex items-center justify-center text-2xl font-bold rounded-md">
-                Binge
+        <NavLink to="/">
+            <div className="w-full h-12 flex items-center justify-center my-2">
+                <div className="text-blue-500 bg-yellow-300 w-20 h-10 flex items-center justify-center text-2xl font-bold rounded-md">
+                    Binge
+                </div>
             </div>
-        </div>
+        </NavLink>
     );
 };
 
