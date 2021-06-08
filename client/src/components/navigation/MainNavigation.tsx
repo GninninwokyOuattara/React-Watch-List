0a1b1e27561495d84983ca3e37724893909dd196
Gninninwoky Ouattara <idrissgouattara@gmail.com>
2021-06-08 00:04:06 +0000

    little adjustement, li tag now inside the NavigationLink component

--- client/src/components/navigation/MainNavigation.tsx
+++ client/src/components/navigation/MainNavigation.tsx
@@ -4,32 +4,30 @@ import { NavLink } from "react-router-dom";
 import Burger from "./Burger";
 
 import { Auth } from "../../context/AuthContext";
+import TextLogo from "../shared/TextLogo";
+import NavigationLink from "./NavigationLink";
 
 const MainNavigation = () => {
     const auth = useContext(Auth);
 
     return (
-        <div className="w-screen h-14 bg-gray-600 flex justify-between items-center px-8 py-2 mb-1">
+        <div className="w-screen h-14 bg-yellow-600 flex justify-between items-center px-8 py-2 mb-1">
             <Burger />
-            <div className="logo text-white">WATCHLIST</div>
-            <ul className="hidden md:flex list-none flex-row">
-                <NavLink to="/" className="mx-3">
-                    My Watchlist
-                </NavLink>
+            <TextLogo text={"Binge"} />
+            <ul className="hidden md:flex list-none flex-row h-full ">
                 {!auth?.isLoggedIn && (
                     <React.Fragment>
-                        <NavLink to="/login" className="mx-3">
-                            Login
-                        </NavLink>
-                        <NavLink to="/register" className="mx-3">
-                            Register
-                        </NavLink>
+                        <NavigationLink to="/login" text="Login" />
+
+                        <NavigationLink to="/register" text="Register" />
                     </React.Fragment>
                 )}
                 {auth?.isLoggedIn && (
-                    <NavLink to="/" className="mx-3">
-                        Logout
-                    </NavLink>
+                    <React.Fragment>
+                        <NavigationLink to="/me" text="My Watchlist" />
+
+                        <NavigationLink text="Logout" type="button" />
+                    </React.Fragment>
                 )}
             </ul>
         </div>
