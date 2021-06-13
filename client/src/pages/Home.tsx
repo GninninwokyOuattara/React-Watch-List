ccfc3ddb56bbfdfbfbb8a70a85b2cf05b4402039
Gninninwoky Ouattara <idrissgouattara@gmail.com>
2021-06-13 10:30:23 +0000

    removed useless test stuff

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,13 +1,11 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import LoginForm from "../shared/components/Login";
 import NavigationBar from "../shared/components/navigation/NavigationBar";
 
 const Home = () => {
     return (
         <React.Fragment>
             <NavigationBar />
-            <LoginForm />
         </React.Fragment>
     );
 };
