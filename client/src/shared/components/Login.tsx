6b76be312edbd2af13dce6bb8e60f5c1ffcaf692
Gninninwoky Ouattara <idrissgouattara@gmail.com>
2021-06-13 00:13:59 +0000

    wrapped by backdrop overlay to keep user from doing other things

--- client/src/shared/components/Login.tsx
+++ client/src/shared/components/Login.tsx
@@ -1,9 +1,14 @@
 import React, { useRef, useState, useContext } from "react";
 import { authContext, userData, authType } from "../context/authContext";
+import BackdropBlack from "../../components/BackdropBlack";
 
 let server_url = process.env.REACT_APP_SERVER_DEV as string;
 
-const LoginForm = () => {
+interface props {
+    onCancel: () => void;
+}
+
+const LoginForm: React.FC<props> = (props) => {
     const emailRef = useRef(null);
     const passwordRef = useRef(null);
     const [formData, setFormData] = useState({});
@@ -38,34 +43,39 @@ const LoginForm = () => {
     };
 
     return (
-        <form
-            action=""
-            className="border h-56 w-96 flex flex-col p-4"
-            onSubmit={submitHandler}
-        >
-            <h1 className="w-full text-center">Login and start Binging </h1>
-            <input
-                id="email"
-                type="text"
-                className="w-full  px-3 h-10 border-b my-2 rounded-md appearance-none outline-none"
-                placeholder="Email"
-                ref={emailRef}
-                onChange={changeHandler}
-            />
-            <input
-                id="password"
-                type="password"
-                className="w-full px-3 h-10 border-b my-2 rounded-md"
-                placeholder="Password"
-                ref={passwordRef}
-                onChange={changeHandler}
-            />
-            <div className="mt-auto flex justify-center">
-                <button type="submit" className="h-10 w-24 border rounded-xl">
-                    Let's Go
-                </button>
-            </div>
-        </form>
+        <BackdropBlack onCancel={props.onCancel}>
+            <form
+                action=""
+                className="border h-56 w-96 flex flex-col p-4 z-10 bg-white"
+                onSubmit={submitHandler}
+            >
+                <h1 className="w-full text-center">Login and start Binging </h1>
+                <input
+                    id="email"
+                    type="text"
+                    className="w-full  px-3 h-10 border-b my-2 rounded-md appearance-none outline-none"
+                    placeholder="Email"
+                    ref={emailRef}
+                    onChange={changeHandler}
+                />
+                <input
+                    id="password"
+                    type="password"
+                    className="w-full px-3 h-10 border-b my-2 rounded-md"
+                    placeholder="Password"
+                    ref={passwordRef}
+                    onChange={changeHandler}
+                />
+                <div className="mt-auto flex justify-center">
+                    <button
+                        type="submit"
+                        className="h-10 w-24 border rounded-xl"
+                    >
+                        Let's Go
+                    </button>
+                </div>
+            </form>
+        </BackdropBlack>
     );
 };
 
