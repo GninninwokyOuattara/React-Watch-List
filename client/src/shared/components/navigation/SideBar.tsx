4762d60d49fe200a513c42866a861066039952be
Gninninwoky Ouattara <idrissgouattara@gmail.com>
2021-06-20 14:17:58 +0000

    two more component to render proper navigation NavigationLink & FontIcon

--- client/src/shared/components/navigation/SideBar.tsx
+++ client/src/shared/components/navigation/SideBar.tsx
@@ -1,27 +1,68 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const SideBar = () => {
     return (
-        <aside className="sidebar w-64 h-full border border-blue-300 flex flex-col px-4">
+        <aside className="hidden md:flex sidebar w-64 h-full border border-blue-300 flex-col px-4">
             <div className="brand my-10">Binge.it</div>
             <ul className="links">
                 <li className="link">
-                    <NavLink to="/">Explorer</NavLink>
+                    <NavLink to="/">
+                        <NavigationLink title="Home" font="fal fa-home" />
+                    </NavLink>
                 </li>
                 <li className="link">
-                    <NavLink to="/">Watchlist</NavLink>
+                    <NavLink to="/">
+                        <NavigationLink
+                            title="Explorer"
+                            font="fal fa-telescope"
+                            mr={2}
+                        />
+                    </NavLink>
                 </li>
                 <li className="link">
-                    <NavLink to="/">Reviews</NavLink>
+                    <NavLink to="/">
+                        <NavigationLink title="Watchlist" font="fal fa-film" />
+                    </NavLink>
                 </li>
                 <li className="link">
-                    <NavLink to="/">Profil</NavLink>
+                    <NavLink to="/">
+                        <NavigationLink
+                            title="Reviews"
+                            font="fal fa-comment-alt"
+                        />
+                    </NavLink>
                 </li>
             </ul>
             <div className="user mt-auto">Coming soon</div>
         </aside>
     );
+};
+
+interface NaviLink {
+    title: string;
+    font?: string;
+    mr?: number;
+}
+
+export const NavigationLink: React.FC<NaviLink> = ({ title, font, mr }) => {
+    return (
+        <div className="flex items-center my-4">
+            {font && <FontIcon font={font} mr={mr} />}
+
+            <div className="link-title">{title}</div>
+        </div>
+    );
+};
+
+interface Font {
+    font: string;
+    mr?: number;
+}
+
+export const FontIcon: React.FC<Font> = ({ font, mr }) => {
+    return <i className={`${font} mr-${mr || 3} fa-2x`}></i>;
 };
 
 export default SideBar;
