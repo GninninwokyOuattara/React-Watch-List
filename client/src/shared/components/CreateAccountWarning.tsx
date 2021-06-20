0fee87713d0802c3eb60a0252df1f0953674b0d7
Gninninwoky Ouattara <idrissgouattara@gmail.com>
2021-06-20 17:39:11 +0000

    register form pop out when click on create account

--- client/src/shared/components/CreateAccountWarning.tsx
+++ client/src/shared/components/CreateAccountWarning.tsx
@@ -1,14 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
+import RegisterForm from "./RegisterForm";
 
 const CreateAccountWarning = () => {
+    const [showRegister, setShowRegister] = useState(false);
+
     return (
-        <div className="not-login border-2 border-gray-500 w-full h-auto p-1 rounded-md py-2">
-            <h2 className="text-center my-1">New on Binge ?</h2>
-            <p>Create an account and prepare to Binge</p>
-            <button className="bg-red-300 rounded-full w-full mt-2 h-10">
-                Create an Account
-            </button>
-        </div>
+        <React.Fragment>
+            {showRegister && (
+                <RegisterForm onCancel={() => setShowRegister(false)} />
+            )}
+            <div className="not-login border-2 border-gray-500 w-full h-auto p-1 rounded-md py-2">
+                <h2 className="text-center my-1">New on Binge ?</h2>
+                <p>Create an account and prepare to Binge</p>
+                <button
+                    className="bg-red-300 rounded-full w-full mt-2 h-10"
+                    onClick={() => setShowRegister(true)}
+                >
+                    Create an Account
+                </button>
+            </div>
+        </React.Fragment>
     );
 };
 
