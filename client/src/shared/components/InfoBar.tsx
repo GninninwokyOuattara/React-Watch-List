12b5b7676489b81dbebe53d4a3d21a9316f34395
Gninninwoky Ouattara <idrissgouattara@gmail.com>
2021-06-20 17:58:10 +0000

    wrapped createAccount component with RegisterProvider

--- client/src/shared/components/InfoBar.tsx
+++ client/src/shared/components/InfoBar.tsx
@@ -1,12 +1,17 @@
 import React, { useContext } from "react";
 import { authContext, authType } from "../context/authContext";
+import RegisterProvider from "../context/RegisterContext";
 import CreateAccountWarning from "./CreateAccountWarning";
 
 const InfoBar = () => {
     const { isLoggedIn } = useContext(authContext) as authType;
     return (
         <aside className="aside aside-right w-96  hidden md:flex md:flex-col pr-2 pt-2">
-            {!isLoggedIn && <CreateAccountWarning />}
+            {!isLoggedIn && (
+                <RegisterProvider>
+                    <CreateAccountWarning />
+                </RegisterProvider>
+            )}
         </aside>
     );
 };
