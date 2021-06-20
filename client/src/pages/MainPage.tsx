b483eaf261510c2c74363b4a9f18bf4e078318eb
Gninninwoky Ouattara <idrissgouattara@gmail.com>
2021-06-20 22:10:44 +0000

    remove element that is now provided by layout component

--- client/src/pages/MainPage.tsx
+++ client/src/pages/MainPage.tsx
@@ -1,22 +1,16 @@
 import React from "react";
-import Header from "../shared/components/ui/Header";
-import Table from "../shared/components/ui/Table";
+import InfoBar from "../shared/components/InfoBar";
+import BottomBar from "../shared/components/navigation/BottomBar";
+import SideBar from "../shared/components/navigation/SideBar";
 
 const MainPage = () => {
     return (
         <React.Fragment>
-            <div className="flex  flex-col h-full py-4">
-                <Header />
-
-                <div className="flex-grow  h-full bg-black-18">
-                    <div className="flex flex-col pt-4 pl-3 h-full">
-                        <h1 className="text-white font-bold text-2xl mb-4">
-                            Top Rate Movies
-                        </h1>
-                        <Table />
-                    </div>
+            <main className="main-content p-2 w-full h-full">
+                <div className="flex flex-col overflow-auto h-full border-2 border-green-500">
+                    Main Content
                 </div>
-            </div>
+            </main>
         </React.Fragment>
     );
 };
