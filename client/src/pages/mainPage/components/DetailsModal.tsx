001e6816657deb32d4ab0c4e9c09a1b965d82d85
Gninninwoky Ouattara <idrissgouattara@gmail.com>
2021-07-20 00:18:20 +0000

    arrow to detailModal component to point at item is it referencing

--- client/src/pages/mainPage/components/DetailsModal.tsx
+++ client/src/pages/mainPage/components/DetailsModal.tsx
@@ -55,8 +55,8 @@ const DetailsModal: React.FC<props> = (props) => {
                         top: props.top,
                         left:
                             props.relativeMiddle - props.left < 0
-                                ? props.left - 290
-                                : props.left + 146,
+                                ? props.left - 295
+                                : props.left + 165,
                     }}
                     ref={divRef}
                 >
@@ -65,10 +65,42 @@ const DetailsModal: React.FC<props> = (props) => {
                             <i className="fad fa-spinner-third fa-10x fa-spin"></i>
                         </div>
                     ) : (
-                        <h2>
-                            {searchData &&
-                                (searchData as movieDataDetails).Plot}
-                        </h2>
+                        <div className="flex p-3 relative">
+                            {props.relativeMiddle - props.left > 0 ? (
+                                <div
+                                    className="arrow-left absolute bottom-0"
+                                    style={{
+                                        width: 0,
+                                        height: 0,
+                                        borderTop: "15px solid transparent",
+                                        borderBottom: "15px solid transparent",
+                                        borderRight: "15px solid red",
+                                        position: "absolute",
+                                        left: "-15px",
+                                        top: "15px",
+                                    }}
+                                ></div>
+                            ) : (
+                                <div
+                                    className="arrow-right absolute bottom-0"
+                                    style={{
+                                        width: 0,
+                                        height: 0,
+                                        borderTop: "15px solid transparent",
+                                        borderBottom: "15px solid transparent",
+                                        borderLeft: "15px solid red",
+                                        position: "absolute",
+                                        right: "-15px",
+                                        top: "15px",
+                                    }}
+                                ></div>
+                            )}
+
+                            <h2>
+                                {searchData &&
+                                    (searchData as movieDataDetails).Plot}
+                            </h2>
+                        </div>
                     )}
                 </div>
             )}
